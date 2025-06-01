9551cae3bca67294619c4924758ffa018c014d84
10-070-AnnisaSalsabila <annisa.123140070@student.itera.ac.id>
2025-06-01 19:06:10 +0700

    Initial commit

---
+++ run-bots.bat
@@ -0,0 +1,6 @@
+@echo off
+start cmd /c "python main.py --logic Salsa --email=salsa@email.com --name=salsa --password=123456 --team etimo"
+start cmd /c "python main.py --logic Yere --email=yere@email.com --name=yere --password=123456 --team etimo"
+start cmd /c "python main.py --logic April --email=april@email.com --name=april --password=123456 --team etimo"
+start cmd /c "python main.py --logic Annisa --email=annisa@email.com --name=annisa --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Logic --email=logic@email.com --name=logic --password=123456 --team etimo"
